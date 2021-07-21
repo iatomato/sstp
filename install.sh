64437fe1869f222b7a81d580292a748d2e8d333f
ISNTL <77953022+iatomato@users.noreply.github.com>
2021-07-21 17:15:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare install_dir=src/service.py
+declare readonly install_dir=src/service.py
 sudo chomd a+x $install_dir && sudo cp $install_dir /usr/sbin/v2ctl
 
 if [ $? -eq 0 ]
