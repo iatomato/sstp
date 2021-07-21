b38e4700063e9f59d09d6c033f8aaa43cbe8c7c0
ISNTL <77953022+iatomato@users.noreply.github.com>
2021-07-21 17:15:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo chomd a+x $install_dir && sudo cp $install_dir /usr/sbin/v2ctl
 
 if [ $? -eq 0 ]
 then
-	echo "install [OK]"
-	echo "Typing v2ctl on console (terminal)"
+	echo "Installation >>> [OK]"
+	echo "Try typing ssv2"
 fi
